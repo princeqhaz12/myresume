e3bb634909b7cffdef570e46924a84f8292216dd
princeqhaz12 <72460515+princeqhaz12@users.noreply.github.com>
2024-11-11 14:05:59 +0300

    initial commit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -21,10 +21,10 @@ import porfolioImage4 from "../images/portfolio/portfolio-4.jpg";
 import porfolioImage5 from "../images/portfolio/portfolio-5.jpg";
 import porfolioImage6 from "../images/portfolio/portfolio-6.jpg";
 import porfolioImage7 from "../images/portfolio/portfolio-7.jpg";
-import porfolioImage8 from "../images/portfolio/portfolio-8.jpg";
+/**import porfolioImage8 from "../images/portfolio/portfolio-8.jpg";
 import porfolioImage9 from "../images/portfolio/portfolio-9.jpg";
 import porfolioImage10 from "../images/portfolio/portfolio-10.jpg";
-import porfolioImage11 from "../images/portfolio/portfolio-11.jpg";
+import porfolioImage11 from "../images/portfolio/portfolio-11.jpg";**/
 import profilepic from "../images/profilepic.jpg";
 import testimonialImage from "../images/testimonial.webp";
 import {
@@ -213,48 +213,48 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: "Project title 1",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "Banners",
+    description: "Illustrator & Photoshop",
+    url: "https://www.pinterest.com/princeqhaz/banners/",
     image: porfolioImage1,
   },
   {
-    title: "Project title 2",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "Product Design",
+    description: "Illustrator & Photoshop",
+    url: "https://www.pinterest.com/princeqhaz/product-design/",
     image: porfolioImage2,
   },
   {
-    title: "Project title 3",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "Carton Design",
+    description: "Illustrator & Photoshop",
+    url: "https://www.pinterest.com/princeqhaz/carton-box-design/",
     image: porfolioImage3,
   },
   {
-    title: "Project title 4",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "Social Media Post",
+    description: "Illustrator & Photoshop",
+    url: "https://www.pinterest.com/princeqhaz/social-media-post/",
     image: porfolioImage4,
   },
   {
-    title: "Project title 5",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "Catalogue",
+    description: "Illustrator, Photoshop, & Indesign",
+    url: "https://www.pinterest.com/princeqhaz/catalogue/",
     image: porfolioImage5,
   },
   {
-    title: "Project title 6",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "UI/UX Design",
+    description: "Figma",
+    url: "https://www.pinterest.com/princeqhaz/ui-designs/",
     image: porfolioImage6,
   },
   {
-    title: "Project title 7",
-    description: "Give a short description of your project here.",
-    url: "https://www.pinterest.com/princeqhaz/",
+    title: "3D",
+    description: "Photoshop & Cinema4D",
+    url: "https://www.pinterest.com/princeqhaz/3d/",
     image: porfolioImage7,
   },
-  {
+  /**{
     title: "Project title 8",
     description: "Give a short description of your project here.",
     url: "https://www.pinterest.com/princeqhaz/",
@@ -278,6 +278,7 @@ export const portfolioItems: PortfolioItem[] = [
     url: "https://www.pinterest.com/princeqhaz/",
     image: porfolioImage11,
   },
+  **/
 ];
 
 /**
